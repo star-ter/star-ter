d86d82e0d5214d322bbd0ceac120d93a5cb24058
장봉익 <bongsik9410@gmail.com>
2025-12-31 02:49:49 +0900

    refactor : analysis code

--- apps/api/src/analysis/analysis.module.ts
+++ apps/api/src/analysis/analysis.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnalysisService } from './analysis.service';
 import { AnalysisController } from './analysis.controller';
+import { AnalysisRepository } from './analysis.repository';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AnalysisController],
-  providers: [AnalysisService],
+  providers: [AnalysisService, AnalysisRepository],
 })
 export class AnalysisModule {}
